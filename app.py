670f8adf681145e365bbf8637d57f7209b8f266a
Ivina Lorena Oliveira Moura <ivinalorena@aluno.unilab.edu.br>
2025-09-26 08:42:54 -0300

    primeiro commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('telainicial.html')
+
+@app.route('/POSCOMP2022')
+def POSCOMP2022():
+    return render_template('POSCOMP2022.html')
+
+@app.route('/POSCOMP2023')
+def POSCOMP2023():
+    return render_template('POSCOMP2023.html')
+
+@app.route('/POSCOMP2024')
+def POSCOMP2024():
+    return render_template('POSCOMP2024.html')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+#@app.route('/POSCOMP2023')
+#def POSCOMP2023():
+#    return render_template('POSCOMP2023.html')
+
+if __name__ == '__main__':
+    app.run
